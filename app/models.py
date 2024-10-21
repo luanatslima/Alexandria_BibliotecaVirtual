944ac3896a15eb81532b6fcc878dde0dae4d0485
Luana <luanatayna2007@gmail.com>
2024-10-21 15:52:45 -0300

    commit recente

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Pagina(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField(max_length=1200)
+
+    def __str__(self):
+        return self.titulo
--- app/views.py
+++ app/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def area_do_usuario(request):
+    return render(request, "area_do_usuario.html")
+
+def configuracoes(request):
+    return render(request, "configuracoes.html")
+
+def meus_materiais(request):
+    return render(request, "meus_materiais.html")
+
+def sobre_nos(request):
+    return render(request, "sobre_nos.html")
