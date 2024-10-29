6addf99422768bfdcadaf86ffc5ab9542f82848a
Luana <luanatayna2007@gmail.com>
2024-10-29 14:49:14 -0300

    Alterações em models

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from . models import Pagina
-
-admin.site.register(Pagina)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class Pagina(models.Model):
-    titulo = models.CharField(max_length=100)
-    conteudo = models.TextField(max_length=1200)
-
-    def __str__(self):
-        return self.titulo
